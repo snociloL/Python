9d453bf8b54b7d4977a88c2a577e929a81573f37
snociloL <aqil.radzi05@gmail.com>
2023-01-19 23:41:18 +0800

    simple dictionaries excercises

--- Composite Types.py
+++ Composite Types.py
@@ -2,7 +2,7 @@
 
 list1 = ['Scheme', 'Prolog', 'C', 'Java', 'Python']
 
-print(list1)
+print("\n", list1)
 print(list1[2])
 print(list1[0:2])
 print(len(list1))
@@ -18,3 +18,17 @@ print(alldDepartments['Networking'])
 
 
 #Task 5 c)
+
+tuple1 = (11, 22,33,44,55,66)
+
+print("\n", tuple1)
+tuple2 = tuple1[3:5]
+print(tuple2)
+
+#Task 6
+
+q3 = {'September': 30, 'October' : 31, 'November' : 30, 'December' : 31}
+
+print("\n", q3)
+print(q3.keys())
+print(q3.values())
