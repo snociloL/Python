e17537cd1eadd0a354f34347dd53bc25779fbe10
snociloL <aqil.radzi05@gmail.com>
2023-01-19 23:10:43 +0800

    Python Lab Excercise

---
+++ Composite Types.py
@@ -0,0 +1,20 @@
+#Task 5 a)
+
+list1 = ['Scheme', 'Prolog', 'C', 'Java', 'Python']
+
+print(list1)
+print(list1[2])
+print(list1[0:2])
+print(len(list1))
+print(list1 + ['DrScheme', 'WinProlog', 'DevC++'])
+
+#Task 5 b)
+
+alldDepartments = {'System' : 16, 'Programming' : 22, 'Networking' : 12}
+
+print("\n", alldDepartments.keys())
+print(alldDepartments.values())
+print(alldDepartments['Networking'])
+
+
+#Task 5 c)
